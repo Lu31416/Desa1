6319d8147a80f3b35d4cf4d5c262cda128f5f246
Luisa Tutau <58300331+Lu31416@users.noreply.github.com>
2023-08-25 20:50:19 -0300

    finalizado

--- tomada de decisao.py
+++ tomada de decisao.py
@@ -1,3 +1,4 @@
+'''
 num1=int(input('ingrese un numero'))
 num2=int(input('ingrese otro numero') )
 
@@ -5,3 +6,24 @@ if num1>=num2:
     print(f'o maior numero e {num1}')
 else:
     print(f'o maior numero e {num2}')
+'''
+'''
+turno = input('ingrese el turno M-matutino ou V-Vespertino ou N- Noturno ')
+
+if turno=="M":
+    print('bom dia')
+elif turno =="V":
+    print('boa tarde')
+elif turno =="N":
+    print('boa noite')
+else:
+    print('valor invalido!')
+'''
+
+while True:
+    nota = int(input(f'ingrese una nota entre zero e dez '))
+    if nota >=0 and nota <=10:
+        print('very good')
+        break
+    else:
+        print('reintente')
