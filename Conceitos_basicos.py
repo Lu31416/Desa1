2a1ad9b65f4e129f4bc154f52ec0db7121ff5dd4
Luisa Tutau <58300331+Lu31416@users.noreply.github.com>
2023-08-25 19:47:38 -0300

    commit de conceitos basicos

---
+++ Conceitos_basicos.py
@@ -0,0 +1,19 @@
+'''
+numero = int(input(f'Ingrese um numero '))
+print(f'o numero informado foi{numero}')
+
+num1 = int(input(f'ingrese numero 1 '))
+num2 = int(input(f'ingrese numero 2 '))
+soma = num1 + num2
+print(f'a soma dos numeros e {soma}')
+'''
+'''
+cels = float(input(f'ingrese temperatura '))
+faren = (cels * 1.8) + 32
+print(f'en farenheit e {faren}')
+'''
+
+num1 = int(input(f'cuanto voce ganha por hora '))
+num2 = int(input(f'cuantas horas voce trabalha '))
+total = num1*num2
+print(f'seu salario e {total}')
