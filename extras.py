2330baf5bcaae1142d15f689ea184f9db66da427
Luisa Tutau <58300331+Lu31416@users.noreply.github.com>
2023-08-27 17:41:46 -0300

    Update extras.py

--- extras.py
+++ extras.py
@@ -1,3 +1,4 @@
+'''
 moeda={
     'dolar_americano' : 4.91,
     'peso_argentino' : 0.02,
@@ -15,6 +16,35 @@ if seleccion in moeda:
     print(f"Você terá aproximadamente {valor_convertido:.2f} {seleccion}")
 else:
     print("Moeda não encontrada na lista.")   
+
+'''
+'''
+salario = int(input('ingrese su salario'))
+
+if salario <= 1000:
+    novosalario = salario*1.2
+elif 1000 < salario < 2800:
+    novosalario = salario*1.1
+else:
+    novosalario = salario*1.05
+
+print(f'seu novo salario sera {novosalario}')
+'''
+
+def leiaInt(mensagem):
+    while True:
+        try:
+            valor = int(input(mensagem))
+            return valor
+        except ValueError:
+            print("Valor inválido. Por favor, digite um número inteiro.")
+
+n = leiaInt('Digite um número inteiro: ')
+print('Você digitou:', n)
+
+
+
+
 
 
 
